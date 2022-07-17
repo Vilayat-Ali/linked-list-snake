d5cac0877bae1f3ff5d46d23cfb5fc1bfec9535c
Syed Vilayat Ali Rizvi <vilayatcodemysite@gmail.com>
2022-07-17 12:07:17 +0530

    Added reverse linked list operation, refined files

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
